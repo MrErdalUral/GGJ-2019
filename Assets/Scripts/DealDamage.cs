3ed6516876daf7ea7cb5de56939883e602cf04d9
xUMR <xUMR@users.noreply.github.com>
2019-01-26 15:48:10 +0300

    Camera shake

--- Assets/Scripts/DealDamage.cs
+++ Assets/Scripts/DealDamage.cs
@@ -3,22 +3,24 @@ using UnityEngine;
 public class DealDamage : MonoBehaviour
 {
     public int DamageAmount;
+    public float ShakeMultiplier;
 
     public LayerMask DamageLayer;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         var otherLayer = 1 << other.gameObject.layer;
-        if ((otherLayer & DamageLayer) == otherLayer)
-        {
-            other.gameObject.GetComponent<Health>().DealDamage(DamageAmount);
-            var otherBody = other.GetComponent<Rigidbody2D>();
-            var body = GetComponent<Rigidbody2D>();
-            if (otherBody && body)
-            {
-                Debug.Log("knockback: " + body.velocity);
-                otherBody.velocity = body.velocity *  2.5f;
-            }
-        }
+        if ((otherLayer & DamageLayer) != otherLayer) return;
+
+        other.gameObject.GetComponent<Health>().DealDamage(DamageAmount);
+        var otherBody = other.GetComponent<Rigidbody2D>();
+        var body = GetComponent<Rigidbody2D>();
+        if (!otherBody || !body) return;
+
+        Debug.Log("knockback: " + body.velocity);
+        otherBody.velocity = body.velocity *  2.5f;
+
+        Camera.main.GetComponent<CameraShake>()
+            .Shake(DamageAmount * ShakeMultiplier, .1f, body.velocity.normalized);
     }
 }
