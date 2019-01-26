bdd86f633571804c57480cda7b291e0dcc3eaf65
MrErdalUral <mrerdalural@gmail.com>
2019-01-27 00:12:52 +0300

    added basic enemy spawners

---
+++ Assets/EnemySeed.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class EnemySeed : MonoBehaviour
+{
+    private Vector2 _startPos;
+    private Vector2 _targetPos;
+
+    public float Speed;
+    private float _travelProgress;
+    public GameObject[] EnemyPrefabs;
+
+    public void SetPositions(Vector2 startPos, Vector2 targetPos)
+    {
+        _startPos = startPos;
+        _targetPos = targetPos;
+    }
+
+    void Update()
+    {
+        _travelProgress += Time.unscaledDeltaTime;
+        transform.position = Vector2.Lerp(_startPos, _targetPos, _travelProgress);
+        if (_travelProgress > 1)
+        {
+            SpawnEnemy();
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        var enemy = Instantiate(EnemyPrefabs[Random.Range(0, EnemyPrefabs.Length)], _targetPos, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public EnemySeed EnemyPrefab;
+
+    public Transform StartPos;
+    public Transform EndPos;
+
+    public float SpawnTime = 3;
+    public Vector2 SpawnTimeRandomRange = new Vector2(0, 1.5f);
+    private float _spawnCooldown;
+
+    public int SpawnCount = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetCooldown();
+    }
+
+    private void ResetCooldown()
+    {
+        _spawnCooldown = SpawnTime + Random.Range(SpawnTimeRandomRange.x, SpawnTimeRandomRange.y);
+        SpawnCount--;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(SpawnCount < 0) gameObject.SetActive(false);
+        _spawnCooldown -= Time.deltaTime;
+        if (_spawnCooldown < 0)
+        {
+            SpawnEnemy();
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        ResetCooldown();
+        var enemy = Instantiate(EnemyPrefab, StartPos.position,
+            Quaternion.identity);
+        enemy.SetPositions(StartPos.position, EndPos.position);
+    }
+}
