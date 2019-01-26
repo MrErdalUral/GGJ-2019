b497466de06998b7fbf617833b78f0e87f6fac60
MrErdalUral <mrerdalural@gmail.com>
2019-01-26 15:43:26 +0300

    Added enemy movement

---
+++ Assets/EnemyMovementController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Movement2D))]
+public class EnemyMovementController : MonoBehaviour
+{
+    private Movement2D _movement;
+    public EnemyMovementType MovementType = EnemyMovementType.chase;
+
+    void Awake()
+    {
+        _movement = GetComponent<Movement2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var player = GameObject.FindGameObjectWithTag("Player");
+        var dir = (player.transform.position-transform.position).normalized;
+        if (MovementType == EnemyMovementType.avoid)
+            dir = -dir;
+        _movement.SetDirection(dir);
+    }
+}
+
+public enum EnemyMovementType
+{
+    chase,avoid
+}
