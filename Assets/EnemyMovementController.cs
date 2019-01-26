a2501fb7fea7d5bfc3d72cc33ca0d2e9bb9ec043
MrErdalUral <mrerdalural@gmail.com>
2019-01-26 16:06:56 +0300

    Added Enemy stun player attack movement

--- Assets/EnemyMovementController.cs
+++ Assets/EnemyMovementController.cs
@@ -7,6 +7,7 @@ public class EnemyMovementController : MonoBehaviour
 {
     private Movement2D _movement;
     public EnemyMovementType MovementType = EnemyMovementType.chase;
+    private float _cooldown;
 
     void Awake()
     {
@@ -16,11 +17,21 @@ public class EnemyMovementController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _cooldown -= Time.deltaTime;
+        if (_cooldown >= 0)
+            return;
         var player = GameObject.FindGameObjectWithTag("Player");
         var dir = (player.transform.position-transform.position).normalized;
         if (MovementType == EnemyMovementType.avoid)
             dir = -dir;
         _movement.SetDirection(dir);
+    }
+
+    public void StopMovement()
+    {
+        _movement.SetDirection(Vector2.zero);
+
+        _cooldown = 1f;
     }
 }
 
