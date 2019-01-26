893a60ac2d7492b58ab5247327114aca964e4596
xUMR <xUMR@users.noreply.github.com>
2019-01-26 16:27:20 +0300

    Logger prefab

--- Assets/Scripts/Logger.cs
+++ Assets/Scripts/Logger.cs
@@ -1,9 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Logger : MonoBehaviour
 {
+    public Text DebugText;
+
     public void Log(string message)
     {
         print(message);
+        if (DebugText)
+            DebugText.text = message;
+    }
+
+    [NaughtyAttributes.Button]
+    public void ClearDebugText()
+    {
+        if (DebugText)
+            DebugText.text = "";
     }
 }
