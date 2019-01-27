bd323755c4ce30d367b5e7b0a47923ac6eeaf127
MrErdalUral <mrerdalural@gmail.com>
2019-01-27 14:28:27 +0300

    reworked rollback changes

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -6,13 +7,31 @@ public class Health : MonoBehaviour
     public int CurrentHp;
 
     public UnityEvent OnDeath;
+    public UnityEvent OnHit;
+    public float EtherialTime;
 
     public void DealDamage(int amount)
     {
         CurrentHp -= amount;
-        if (CurrentHp < 0)
+        if (CurrentHp <= 0)
         {
             OnDeath.Invoke();
         }
+        else
+        {
+            OnHit.Invoke();
+        }
+    }
+    public void BecomeGhost()
+    {
+        StartCoroutine(BecomeGhostRoutine());
+    }
+
+    private IEnumerator BecomeGhostRoutine()
+    {
+        var originalLayer = gameObject.layer;
+        gameObject.layer = LayerMask.NameToLayer("Ghost");
+        yield return new WaitForSeconds(EtherialTime);
+        gameObject.layer = originalLayer;
     }
 }
